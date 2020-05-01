b92e0f3e9f9c2b562746d316c4b74ac6ab7c4c13
Chandu sanjith <chandusanjith.t.2610@gmail.com>
2020-05-01 15:02:19 +0000

    Initial Release

--- mysite/VirusTotal.py
+++ mysite/VirusTotal.py
@@ -3,7 +3,11 @@ import csv
 
 def VirusTotalChecker(ip):
     vt = virustotal2.VirusTotal2("b2510b80fec019d8b6896a8e575022690efecdfa858d1077c75b37dae5f4621e")
-    ip_report = vt.retrieve(ip)
+    try:
+        ip_report = vt.retrieve(ip)   #get the VT IP report for this IP
+    except:
+        return("API error: on ip " + ip)
+    
     total_pos = sum([u["positives"] for u in ip_report.detected_urls])
     total_scan = sum([u["total"] for u in ip_report.detected_urls])
     count = len(ip_report.detected_urls)
