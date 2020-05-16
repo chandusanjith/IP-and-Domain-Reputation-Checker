aef01cf27dcc68dff3b032a211185cc7f16928c8
Chandu sanjith <chandusanjith.t.2610@gmail.com>
2020-05-16 13:24:44 +0000

    error fixes

--- mysite/settings.py
+++ mysite/settings.py
@@ -78,23 +78,12 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
-#DATABASES = {
-#    'default': {
-#        'ENGINE': 'django.db.backends.sqlite3',
-#        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-#    }
-#}
-
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'd48m2ob38sm235',
-        'USER': 'yfpysdweqzcenb',
-        'PASSWORD': '2b6495d853dc866cf773278624808a4a5614fd56f2172202344699d936401b28',
-        'HOST': 'ec2-18-233-32-61.compute-1.amazonaws.com',
-        'PORT': '5432',
-    }
-}
+   'default': {
+       'ENGINE': 'django.db.backends.sqlite3',
+       'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }}
+
 
 # Password validation
 # https://docs.djangoproject.com/en/3.0/ref/settings/#auth-password-validators
