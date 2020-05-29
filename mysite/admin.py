a44bfb85316f6136b2f67c419c20a6397293eaca
Chandu sanjith <chandusanjith.t.2610@gmail.com>
2020-05-29 12:24:43 +0000

    Admin level changes

--- mysite/admin.py
+++ mysite/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
-from .models import ips,contacted, IPData, CountryData
+from .models import ips,contacted, IPData, CountryData, Hashes
 
 admin.site.register(ips)
 admin.site.register(contacted)
 admin.site.register(IPData)
 admin.site.register(CountryData)
+admin.site.register(Hashes)
